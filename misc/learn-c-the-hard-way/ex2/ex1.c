2a4924423b65fc311844e09e9d00789d3f3b8e48
Zhu Guiyuan <zhugy2022@shanghaitech.edu.cn>
2024-08-18 22:33:31 +0800

    Learn-C: Complete ex2

---
+++ misc/learn-c-the-hard-way/ex2/ex1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  puts("The Zen of Python, by Tim Peters");
+  puts("");
+  puts("Beautiful is better than ugly.");
+  puts("Explicit is better than implicit.");
+  puts("Simple is better than complex.");
+  puts("Complex is better than complicated.");
+  puts("Flat is better than nested.");
+  puts("Sparse is better than dense.");
+  puts("Readability counts.");
+  puts("Special cases aren't special enough to break the rules.");
+  puts("Although practicality beats purity.");
+  puts("Errors should never pass silently.");
+  puts("Unless explicitly silenced.");
+  puts("In the face of ambiguity, refuse the temptation to guess.");
+  puts("There should be one-- and preferably only one --obvious way to do it.");
+  puts("Although that way may not be obvious at first unless you're Dutch.");
+  puts("Now is better than never.");
+  puts("Although never is often better than *right* now.");
+  puts("If the implementation is hard to explain, it's a bad idea.");
+  puts("If the implementation is easy to explain, it may be a good idea.");
+  puts("Namespaces are one honking great idea -- let's do more of those!");
+  return 0;
+}
