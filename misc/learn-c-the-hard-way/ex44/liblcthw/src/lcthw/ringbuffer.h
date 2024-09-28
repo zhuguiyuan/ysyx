11c82787fa7e15fc80b36e97b7da59c0c870e250
Zhu Guiyuan <zhugy2022@shanghaitech.edu.cn>
2024-09-29 01:16:45 +0800

    Learn-C: add comments in the header for ex44

--- misc/learn-c-the-hard-way/ex44/liblcthw/src/lcthw/ringbuffer.h
+++ misc/learn-c-the-hard-way/ex44/liblcthw/src/lcthw/ringbuffer.h
@@ -1,3 +1,10 @@
+/**
+ * Comment by Guiyuan Zhu:
+ *
+ *   This is not really a "ring" buffer. Feilds `start` and `end` only are set
+ *   to 0 when the buffer is empty. So don't be suprised by the implementation
+ *   of the macros (RingBuffer_available_data and RingBuffer_available_space).
+ */
 #ifndef _lcthw_RingBuffer_h
 #define _lcthw_RingBuffer_h
 
@@ -18,37 +25,39 @@ int RingBuffer_read(RingBuffer *buffer, char *target, int amount);
 
 int RingBuffer_write(RingBuffer *buffer, char *data, int length);
 
-int RingBuffer_empty(RingBuffer *buffer);
-
-int RingBuffer_full(RingBuffer *buffer);
-
-int RingBuffer_available_data(RingBuffer *buffer);
-
-int RingBuffer_available_space(RingBuffer *buffer);
-
 bstring RingBuffer_gets(RingBuffer *buffer, int amount);
 
+/** Check how many space we could read from the ringbuffer. */
 #define RingBuffer_available_data(B)                                           \
   (((B)->end + 1) % (B)->length - (B)->start - 1)
 
+/** Check how many space we could write before we clean the ringbuffer. */
 #define RingBuffer_available_space(B) ((B)->length - (B)->end - 1)
 
+/** Check whether buffer is full */
 #define RingBuffer_full(B) (RingBuffer_available_data((B)) - (B)->length == 0)
 
+/** Check whether buffer is empty */
 #define RingBuffer_empty(B) (RingBuffer_available_data((B)) == 0)
 
+/** Puts char * data D into buffer B */
 #define RingBuffer_puts(B, D) RingBuffer_write((B), bdata((D)), blength((D)))
 
+/** Get all data from buffer B. The return value is bstring type. */
 #define RingBuffer_get_all(B)                                                  \
   RingBuffer_gets((B), RingBuffer_available_data((B)))
 
+/** Get the char* pointer to the buffer start address. */
 #define RingBuffer_starts_at(B) ((B)->buffer + (B)->start)
 
+/** Get the char* pointer to the buffer end address. */
 #define RingBuffer_ends_at(B) ((B)->buffer + (B)->end)
 
+/** Commit buffer B with a read action of amount A without any bound check. */
 #define RingBuffer_commit_read(B, A)                                           \
   ((B)->start = ((B)->start + (A)) % (B)->length)
 
+/** Commit buffer B with a write action of amount A without any bound check. */
 #define RingBuffer_commit_write(B, A)                                          \
   ((B)->end = ((B)->end + (A)) % (B)->length)
 
